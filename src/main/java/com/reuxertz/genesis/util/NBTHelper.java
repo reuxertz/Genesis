7ec9c5a4cfc1c557731692026bdadd29492b10ef
rjwallace <rjwallace@outlook.com>
2018-07-08 07:57:54 -0500

    added crow texture

--- src/main/java/com/reuxertz/genesis/util/NBTHelper.java
+++ src/main/java/com/reuxertz/genesis/util/NBTHelper.java
@@ -1,6 +1,25 @@
 package com.reuxertz.genesis.util;
 
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+
 public class NBTHelper {
 
+    public static void writeInventory(NBTTagCompound nbtTagCompound, InventoryBasic inventoryBasic)
+    {
+        NBTTagCompound inventoryNBT = new NBTTagCompound();
+
+        for (int i = 0; i < inventoryBasic.getSizeInventory(); i++)
+        {
+            ByteBufUtils.writeItemStack(null, inventoryBasic.getStackInSlot(i));
+        }
+
+    }
+
+    public static InventoryBasic readInventory(NBTTagCompound nbtTagCompound, InventoryBasic inventoryBasic)
+    {
+        return null;
+    }
 
 }
